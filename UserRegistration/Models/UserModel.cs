6072bf0cf8fcf7e8cc61ff94365301b2c8c9ad59
vasuklk <tirumala.kulkarni97@gmail.com>
2023-03-20 00:04:59 +0530

    Removed ? in model file

--- UserRegistration/Models/UserModel.cs
+++ UserRegistration/Models/UserModel.cs
@@ -8,15 +8,15 @@ namespace UserRegistration.Models
         [Required]
         public string UserName { get; set; } = "vasuklk";
         [Required]
-        public string? FirstName { get; set; } = "Vasu";
-        public string? LastName { get; set; } = "Kulkarni";
-        public string? Address { get; set; } = "8-11-184/235 Krishnadevaraya Nagar";
+        public string FirstName { get; set; } = "Vasu";
+        public string LastName { get; set; } = "Kulkarni";
+        public string Address { get; set; } = "8-11-184/235 Krishnadevaraya Nagar";
         [RegularExpression("^([0-9]{10})$", ErrorMessage = "Invalid Mobile Number.")]
         public long Mobile { get; set; } = 8123980995;
         [Required]
-        public string? Password { get; set; } = "123456";
+        public string Password { get; set; } = "123456";
         [Required]
         [EmailAddress]
-        public string? Email { get; set; } = "vasu@gmail.com";
+        public string Email { get; set; } = "vasu@gmail.com";
     }
 }
