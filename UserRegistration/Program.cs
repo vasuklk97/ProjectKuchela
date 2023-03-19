030d4c9932bfe202f103fe64b34cb11748d1786d
vasuklk <tirumala.kulkarni97@gmail.com>
2023-03-20 00:05:38 +0530

    Added Swagger UI for non dev region

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -23,7 +23,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
